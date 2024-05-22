7aa69a72166653f0cfc4007075f1e7f31f89018d
jagadeeshkommoju97 <jagadeeshkommoju97@gmail.com>
2024-05-22 04:01:18 -0400

    Module 1 Some Change adding login page using Reactjs pending

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,13 +6,5 @@ from django.conf import settings
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
-
-    # path for login
-    # path(route='login', view=views.login_user, name='login'),
-    path(route='login', view=views.login_user, name='login'),
-    # path for dealer reviews view
-
-    # path for add a review view
-
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
